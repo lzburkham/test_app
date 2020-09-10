dafddfea6646d867fe5022cc73bbf09ce02a62e0
Logan Burkham <logan.burkham@snhu.edu>
2020-09-10 16:03:34 -0500

    test 11

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [general]\n\
 email = \"logan.z.burkham@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+“ > ~/.streamlit/config.toml
